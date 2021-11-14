32682c38a479aaa91c79dae3af6f9d1fc300f80d
echils <echils@outlook.com>
2021-11-14 23:25:24 +0800

    主键查询功能拓展

--- src/main/java/com/github/mybatis/statement/loader/SelectByPrimaryKeyStatementLoader.java
+++ src/main/java/com/github/mybatis/statement/loader/SelectByPrimaryKeyStatementLoader.java
@@ -4,10 +4,19 @@ import com.github.mybatis.specification.SpecificationMapper;
 import com.github.mybatis.statement.metadata.MappedMetaData;
 import com.github.mybatis.statement.metadata.TableMetaData;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.ibatis.builder.StaticSqlSource;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.ibatis.mapping.SqlCommandType;
 import org.apache.ibatis.mapping.SqlSource;
+import org.apache.ibatis.scripting.xmltags.DynamicSqlSource;
+import org.apache.ibatis.scripting.xmltags.MixedSqlNode;
+import org.apache.ibatis.scripting.xmltags.SqlNode;
+import org.apache.ibatis.scripting.xmltags.StaticTextSqlNode;
 import org.apache.ibatis.session.Configuration;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static com.github.mybatis.MybatisExpandContext.KEYWORDS_ESCAPE_FUNCTION;
 
 /**
  * 通过主键查询功能加载器
@@ -33,8 +42,18 @@ public class SelectByPrimaryKeyStatementLoader extends AbstractExpandStatementLo
         Configuration configuration =
                 mappedMetaData.getMapperFactoryBean().getSqlSession().getConfiguration();
         TableMetaData tableMetaData = mappedMetaData.getTableMetaData();
+        List<SqlNode> sqlNodes = new LinkedList<>();
+        sqlNodes.add(new StaticTextSqlNode("SELECT "));
+        sqlNodes.add(new StaticTextSqlNode(mappedMetaData.getBaseColumnList()));
+        sqlNodes.add(new StaticTextSqlNode(" FROM "
+                + KEYWORDS_ESCAPE_FUNCTION.apply(tableMetaData.getName())));
+        sqlNodes.add(new StaticTextSqlNode(" WHERE 1=1 "));
+        if (StringUtils.isNotBlank(mappedMetaData.getWhereClause())) {
+            sqlNodes.add(new StaticTextSqlNode(" AND " + mappedMetaData.getWhereClause()));
+        }
 
-        return new StaticSqlSource(configuration, null);
+
+        return new DynamicSqlSource(configuration, new MixedSqlNode(sqlNodes));
     }
 
     @Override
